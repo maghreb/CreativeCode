2b6e0e1b57c9aa2f26e5aff4c59228d00f73270a
Court <courtneyjtaylor2015@gmail.com>
2020-05-06 18:08:27 -0700

    Landons Code

--- P5js/TemplateFolder/scripts/sketch.js
+++ P5js/TemplateFolder/scripts/sketch.js
@@ -1,12 +1,46 @@
+let video;
+let poseNet;
+let pose; 
+
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  createCanvas(640,480);
+  video = createCapture(VIDEO);
+  video.hide()
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function draw() {
+function gotPoses(poses) {
+  console.log(poses); 
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+  }
 }
 
-// HELPER STUFF:
+function modelLoaded(){
+  console.log('poseNet ready');
+}
+  
+function draw(){
+  image (video, 0, 0)
+  
+  if (pose){
+    
+    let eyeR = pose.rightEye;
+    let eyeL = pose.leftEye;
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+    
+    fill(213, 0, 0);
+    let nose = pose['nose'];
+    ellipse(nose.x, nose.y, 20, 20);
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    fill(200, 100, 12)
+    let leftWrist = pose['leftWrist'];
+    ellipse(leftWrist.x, leftWrist.y, 30,30);
+            
+    fill(200, 100, 12)
+    let rightWrist = pose['rightWrist'];
+    ellipse(rightWrist.x, rightWrist.y, 30,30);
+  }
 }
